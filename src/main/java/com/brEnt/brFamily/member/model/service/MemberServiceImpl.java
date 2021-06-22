5ab97ffad1350452a6ddf3315f83189fac98e6f0
yebinJeong <82797570+yebinJeong@users.noreply.github.com>
2021-06-22 16:29:29 +0900

    member

--- src/main/java/com/brEnt/brFamily/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/brEnt/brFamily/member/model/service/MemberServiceImpl.java
@@ -1,5 +1,52 @@
 package com.brEnt.brFamily.member.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brEnt.brFamily.member.model.dao.MemberDao;
+import com.brEnt.brFamily.member.model.vo.Member;
+
+//@Component
+@Service // Service의 역할을 가진 클래스라는 걸 보여주기 위한 어노테이션
 public class MemberServiceImpl implements MemberService {
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private MemberDao mDao;
+	
+	@Override
+	public Member loginMember(Member m) {
+		
+		//Member loginUser = mDao.loginMember(sqlSession, m);
+		//return loginUser;
+		return mDao.loginMember(sqlSession, m);
+	}
+
+	@Override
+	public int insertMember(Member m) {
+		//int result = mDao.insertMember(sqlSession, m);
+		//return result;
+		return mDao.insertMember(sqlSession, m);
+	}
+
+	@Override
+	public int updateMember(Member m) {
+		return mDao.updateMember(sqlSession, m);
+	}
+
+	@Override
+	public int deleteMember(String userId) {
+		return mDao.deleteMember(sqlSession, userId);
+	}
+
+	@Override
+	public int idCheck(String userId) {
+		return mDao.idCheck(sqlSession, userId);
+	}
+
 }
+
+
--- src/main/java/com/brEnt/brFamily/member/model/vo/Member.java
+++ src/main/java/com/brEnt/brFamily/member/model/vo/Member.java
@@ -1,7 +1,5 @@
 package com.brEnt.brFamily.member.model.vo;
 
-
- 
 public class Member {
 
 	private int memNo;
@@ -26,6 +24,223 @@ public class Member {
 	private String acceptStatus;
 	private String memStatus;
 	private String adminStatus;
+
+	public Member() {}
+
+	public Member(int memNo, int deptNo, int posiNo, String id, String pwd, String officeEmail, String personalEmail,
+			String memName, String phone, String post, String roadAddress, String detailAddress, String memEnrollDate,
+			String hireDate, String regi, String bank, String account, String profile, int offCount,
+			String acceptStatus, String memStatus, String adminStatus) {
+		super();
+		this.memNo = memNo;
+		this.deptNo = deptNo;
+		this.posiNo = posiNo;
+		this.id = id;
+		this.pwd = pwd;
+		this.officeEmail = officeEmail;
+		this.personalEmail = personalEmail;
+		this.memName = memName;
+		this.phone = phone;
+		this.post = post;
+		this.roadAddress = roadAddress;
+		this.detailAddress = detailAddress;
+		this.memEnrollDate = memEnrollDate;
+		this.hireDate = hireDate;
+		this.regi = regi;
+		this.bank = bank;
+		this.account = account;
+		this.profile = profile;
+		this.offCount = offCount;
+		this.acceptStatus = acceptStatus;
+		this.memStatus = memStatus;
+		this.adminStatus = adminStatus;
+	}
+
+	public int getMemNo() {
+		return memNo;
+	}
+
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+
+	public int getDeptNo() {
+		return deptNo;
+	}
+
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+
+	public int getPosiNo() {
+		return posiNo;
+	}
+
+	public void setPosiNo(int posiNo) {
+		this.posiNo = posiNo;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getOfficeEmail() {
+		return officeEmail;
+	}
+
+	public void setOfficeEmail(String officeEmail) {
+		this.officeEmail = officeEmail;
+	}
+
+	public String getPersonalEmail() {
+		return personalEmail;
+	}
+
+	public void setPersonalEmail(String personalEmail) {
+		this.personalEmail = personalEmail;
+	}
+
+	public String getMemName() {
+		return memName;
+	}
+
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	public String getRoadAddress() {
+		return roadAddress;
+	}
+
+	public void setRoadAddress(String roadAddress) {
+		this.roadAddress = roadAddress;
+	}
+
+	public String getDetailAddress() {
+		return detailAddress;
+	}
+
+	public void setDetailAddress(String detailAddress) {
+		this.detailAddress = detailAddress;
+	}
+
+	public String getMemEnrollDate() {
+		return memEnrollDate;
+	}
+
+	public void setMemEnrollDate(String memEnrollDate) {
+		this.memEnrollDate = memEnrollDate;
+	}
+
+	public String getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public String getRegi() {
+		return regi;
+	}
+
+	public void setRegi(String regi) {
+		this.regi = regi;
+	}
+
+	public String getBank() {
+		return bank;
+	}
+
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public int getOffCount() {
+		return offCount;
+	}
+
+	public void setOffCount(int offCount) {
+		this.offCount = offCount;
+	}
+
+	public String getAcceptStatus() {
+		return acceptStatus;
+	}
+
+	public void setAcceptStatus(String acceptStatus) {
+		this.acceptStatus = acceptStatus;
+	}
+
+	public String getMemStatus() {
+		return memStatus;
+	}
+
+	public void setMemStatus(String memStatus) {
+		this.memStatus = memStatus;
+	}
+
+	public String getAdminStatus() {
+		return adminStatus;
+	}
+
+	public void setAdminStatus(String adminStatus) {
+		this.adminStatus = adminStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [memNo=" + memNo + ", deptNo=" + deptNo + ", posiNo=" + posiNo + ", id=" + id + ", pwd=" + pwd
+				+ ", officeEmail=" + officeEmail + ", personalEmail=" + personalEmail + ", memName=" + memName
+				+ ", phone=" + phone + ", post=" + post + ", roadAddress=" + roadAddress + ", detailAddress="
+				+ detailAddress + ", memEnrollDate=" + memEnrollDate + ", hireDate=" + hireDate + ", regi=" + regi
+				+ ", bank=" + bank + ", account=" + account + ", profile=" + profile + ", offCount=" + offCount
+				+ ", acceptStatus=" + acceptStatus + ", memStatus=" + memStatus + ", adminStatus=" + adminStatus + "]";
+	}
 	
 	
 	

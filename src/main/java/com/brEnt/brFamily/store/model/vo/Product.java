6d5da74b2b31535dc4508dda9bcc229e5bde671d
hamvely <71883439+hamvely@users.noreply.github.com>
2021-06-22 14:14:12 +0900

    Docs : store 관련 문서 추가 및 수정
    Docs : store 관련 문서 추가 및 수정
    author : 김혜미

--- src/main/java/com/brEnt/brFamily/store/model/vo/Product.java
+++ src/main/java/com/brEnt/brFamily/store/model/vo/Product.java
@@ -1,5 +1,26 @@
 package com.brEnt.brFamily.store.model.vo;
 
-public class Product {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+	
+	private int pdtNo;
+	private String pdtCtg;
+	private String pdtFile;
+	private String pdtName;
+	private String pdtDetail;
+	private int orgPrice;
+	private int memPrice;
+	private int pdtStock;
+	private String pdtEnrolldate;
+	private String pdtStatus;
+	
 }

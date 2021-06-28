be22dd42da2fea41f49878b7c13c5772b034ad04
sun-hee-0 <81214024+sun-hee-0@users.noreply.github.com>
2021-06-28 23:33:05 +0900

    Docs : ElecApproval 문서 수정
    Docs : ElecApproval 문서 수정
    author : 최선희

--- src/main/java/com/brEnt/brFamily/elecApproval/model/service/ElecApprovalService.java
+++ src/main/java/com/brEnt/brFamily/elecApproval/model/service/ElecApprovalService.java
@@ -6,8 +6,8 @@ import com.brEnt.brFamily.elecApproval.model.vo.ElecApproval;
 
 public interface ElecApprovalService {
    
-   // 작성자 : 최선희 -- 전자결재 전체 리스트 조회 
-   ArrayList<ElecApproval> selectApprovalTotalList(); 
+	// 작성자 : 최선희 -- 전자결재 전체 리스트 조회
+    ArrayList<ElecApproval> selectApprovalTotalList(); 
    
    
 }

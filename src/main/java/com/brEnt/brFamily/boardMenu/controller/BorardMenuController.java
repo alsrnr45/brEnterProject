5df8b2eb87b3f390b7907e75cc4010f9f263e0fd
박지은 <pjeun1216@daum.net>
2021-07-04 19:25:42 +0900

    docs: BoardMenuController 문서 추가
    docs : BoardMenuController 문서 추가
    author : 박지은

---
+++ src/main/java/com/brEnt/brFamily/boardMenu/controller/BorardMenuController.java
@@ -0,0 +1,15 @@
+package com.brEnt.brFamily.boardMenu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BorardMenuController {
+
+	@RequestMapping()
+	public String selectBoardFree(Model model) {
+	
+		return null;
+	}
+}
